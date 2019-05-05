690a4c87f2fb562b64cb64447da61bd4528ed892
vithuan <49746357+vithuan@users.noreply.github.com>
2019-05-05 11:59:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const PORT = 3484;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+
+var http = require('http');
+var express = require('express');							//#include thư viện express - dùng để tạo server http nhanh hơn thư viện http cũ
+var socketio = require('socket.io')				//#include thư viện socketio
+
+var ip = require('ip');
+var app = express();									//#Khởi tạo một chương trình mạng (app)
+var server = http.Server(app)
+
+var io = socketio(server);			
 console.log("Dang chay")
