ac88aa16c503f181b19e0567592b55642ea1ed51
vithuan <49746357+vithuan@users.noreply.github.com>
2019-05-05 12:04:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
 var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
 webapp_nsp.use(middleware);	
-server.listen(process.env.PORT || PORT);	
+//server.listen(process.env.PORT || PORT);	
 
-console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
+console.log("ok");
